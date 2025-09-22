7dc91957fea6f6f95f4a2202c87a125319a11edf
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-09-22 00:58:10 +0000

    header追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
+  devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :edit, :update, :index, :show, :destroy] 
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
