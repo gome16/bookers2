8bfb2d0e221f41866737c8b2a036312e11ad4cd6
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-09-23 02:18:39 +0000

    [Add] ユーザー情報と投稿機能のパーシャル実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,14 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @user = current_user
-    @book = Book.new
+    @newbook = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to book_path(@book)
+    @newbook = Book.new(book_params)
+    @newbook.user_id = current_user.id
+    if @newbook.save
+      redirect_to book_path(@newbook)
     else
       redirect_to books_path
     end
@@ -18,6 +18,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @newbook = Book.new
   end
 
   def edit
