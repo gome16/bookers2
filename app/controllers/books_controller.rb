41205497029d10022cb87708b3e0319fdb6b68c4
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-09-23 01:02:12 +0000

    [Add] books/indexページの作成と投稿機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,18 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      redirect_to books_path
+    end
   end
 
   def show
@@ -7,4 +20,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
