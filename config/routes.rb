7c518c7cb29ab11d9dad33be89afdca2c9da7fe5
Ubuntu <ubuntu@ip-172-31-4-103.ap-northeast-1.compute.internal>
2025-10-05 23:01:54 +0000

    [Add] コメント用ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :edit, :update, :index, :show, :destroy] do
     resource :favorite, only: [:create, :destroy]
+    resources :book_comments, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
